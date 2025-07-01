0fd04b61b61980d6aa7d0ff8b2ca6b95f408d06d
Wendell Rodrigues <wendellra@ciandt.com>
2025-06-30 21:36:18 -0300

    feat: config proxy e add dependencia json-server

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
+    products:any[] = []
+
+  http = inject(HttpClient);
+
+
+  ngOnInit() {
+    this.http.get('/api/products').subscribe({
+      next: (data) => {
+        this.products = data as any[];
+      },
+      error: (error) => {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    });
+  }
 }
