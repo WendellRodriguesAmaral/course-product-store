e7140977ec24e0a1e22c27acd2d2468c875bcd76
Wendell Rodrigues <wendellra@ciandt.com>
2025-07-16 21:02:23 -0300

    feat: criação component de criar produto e criação de rota via lazing loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './feature/list/list.component';
+import { CreateComponent } from './feature/create/create.component';
 
 export const routes: Routes = [
     {
         path: '',
         component:ListComponent
+    },
+    {
+        path: 'create-product',
+        loadComponent: ()=> import('./feature/create/create.component').then(m => m.CreateComponent)
+        
     }
 ];
