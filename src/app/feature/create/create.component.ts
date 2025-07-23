0e96e020ef662fe7418ad8e0ccad5c8d7d55f8a6
Wendell Rodrigues <wendellrodrigues97@hotmail.com>
2025-07-23 18:17:19 -0300

    feat: criação de logica para cadastrar produto

--- src/app/feature/create/create.component.ts
+++ src/app/feature/create/create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -13,6 +14,8 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class CreateComponent {
 
+productService = inject(ProductsService);
+
 form = new FormGroup({
   title: new FormControl<string>('', 
     { 
@@ -22,8 +25,11 @@ form = new FormGroup({
   ),
 });
 
-onSubmit(){
-  this.form.controls.title;
+onSubmit(){ 
+  this.productService.post({title:this.form.controls.title.value})
+  .subscribe(()=>{
+    alert('Produto criado com sucesso');
+  })
 }
 
 }
--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
+import { Product } from './../models/product.interface';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Product } from '../models/product.interface';
+import { PayloadProduct } from '../models/payload-product.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,13 @@ export class ProductsService {
 
   http = inject(HttpClient);
 
-
   getAll(){
     return this.http.get<Product[]>('/api/products')
+  }
+
+  post(payload:PayloadProduct){
+    return this.http.post('/api/products', payload)
+
   }
   
 
