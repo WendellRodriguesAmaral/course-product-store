65e7fd4b6391a048c79e40adcb920241e7eaf985
Wendell Rodrigues <wendellrodrigues97@hotmail.com>
2025-08-14 15:54:41 -0300

    feat: criando botão de voltar

--- src/app/feature/create/create.component.ts
+++ src/app/feature/create/create.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/models/product.interface';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, RouterLink, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
